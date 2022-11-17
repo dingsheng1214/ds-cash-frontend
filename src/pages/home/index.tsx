105a10ae327cb8cb4859851583715e88c45d58ae
dingsheng <dingduoduo0916@gmail.com>
2022-11-17 20:50:05 +0800

    feat: 账单列表下拉刷新,上拉加载更多

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,35 +1,72 @@
 import {OneDayBills} from '#/global'
 import {fetchBillList} from '@/api/bill'
+import {InfiniteScroll, PullToRefresh, Toast} from 'antd-mobile'
 import dayjs from 'dayjs'
 import {useEffect, useState} from 'react'
 import BillItem from './BillItem'
 import s from './home.module.scss'
 
 export default function Home() {
-  const [expense, setExpense] = useState(0)
-  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0) // 总支出
+  const [income, setIncome] = useState(0) // 总收入
   const [date, setDate] = useState(dayjs().format('YYYY-MM')) // 当前筛选时间
   const [page, setPage] = useState(1) // 分页
-  const [list, setList] = useState<OneDayBills[]>([]) // 账单列表
+  const [oneDayBills, setOneDayBills] = useState<OneDayBills[]>([]) // 账单列表
   const [totalPage, setTotalPage] = useState(0) // 分页总数
 
+  /**
+   * 获取账单列表
+   */
   const getBillList = async () => {
     const {
       data: {list, total_page, total_expense, total_income},
     } = await fetchBillList({
       date,
       user_id: 'a64dbe53-c7ba-49a0-982f-b7d4bd00085a',
-      pageInfo: {page, page_size: 10},
+      pageInfo: {page, page_size: 2},
     })
-    setList(list)
-    setTotalPage(totalPage)
+    if (page === 1) {
+      setOneDayBills(list)
+    } else {
+      setOneDayBills(oneDayBills.concat(list))
+    }
+    setTotalPage(total_page)
     setIncome(total_income)
     setExpense(total_expense)
   }
 
+  /**
+   * 下拉刷新
+   */
+  const refresh = async () => {
+    console.log('refresh')
+    setPage(1)
+    await getBillList()
+  }
+
+  /**
+   * 上拉加载更多
+   */
+  const loadMore = async () => {
+    console.log('loadMore')
+    setPage(page + 1)
+    await getBillList()
+  }
+
+  /**
+   * 首次请求
+   */
   useEffect(() => {
-    getBillList()
-  }, [page])
+    ;(async () => {
+      const loadingToast = Toast.show({
+        icon: 'loading',
+        content: '加载中...',
+        duration: 0,
+      })
+      await getBillList()
+      loadingToast.close()
+    })()
+  }, [])
 
   return (
     <div className={s.home}>
@@ -53,9 +90,12 @@ export default function Home() {
       </div>
 
       <div className={s.list}>
-        {list.map((item) => (
-          <BillItem oneDayBills={item} key={item.date} />
-        ))}
+        <PullToRefresh onRefresh={refresh}>
+          {oneDayBills.map((item, index) => (
+            <BillItem oneDayBills={item} key={index} />
+          ))}
+          <InfiniteScroll loadMore={loadMore} hasMore={page <= totalPage} />
+        </PullToRefresh>
       </div>
     </div>
   )
